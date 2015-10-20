0f7367bd5b6c37395cd4b51b35eb25af08d3fcff
root <thevuuranusls@gmail.com>
2015-10-20 14:25:52 +0700

    initial source from original: build success with thrift 0.9.0, boost 1.57

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=bin
+CND_ARTIFACT_NAME_Debug=xscribe
+CND_ARTIFACT_PATH_Debug=bin/xscribe
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=scribext.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/scribext.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=scribext
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/scribext
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=scribext.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/scribext.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
